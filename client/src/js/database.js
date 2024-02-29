c0743323d9e939417ffd0be4485ee823e507fb76
xwhoknowz <jeremiah2498@gmail.com>
2024-02-29 00:20:17 -0600

    added logic for post and get methods on database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,25 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const jateDb = opendDB(`jate`, 1);
+  const trans = jateDb.transaction(`jate`, `readWrite`)
+  const jateStore = trans.objectStore(`jate`)
+  const request = jateStore.add({ jate: content })
+  const result = await request;
+  console.log(`result.value`, result);
+  // console.error('putDb not implemented');
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const jateDb = opendDB(`jate`, 1);
+  const trans = jateDb.transaction(`jate`, `readWrite`)
+  const jateStore = trans.objectStore(`jate`)
+  const request = jateStore.getAll()
+  const result = await request;
+  console.log(`result.value`, result);
+  // console.error('getDb not implemented');
+}
 
 initdb();
